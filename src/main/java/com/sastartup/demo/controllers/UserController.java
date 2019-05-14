52b59d356b273e7a9b432f59119020f41d11da0e
laroblesaguilar <laroblesaguilar@gmail.com>
2019-05-14 13:19:13 -0500

    forms and controllers updated

--- src/main/java/com/sastartup/demo/controllers/UserController.java
+++ src/main/java/com/sastartup/demo/controllers/UserController.java
@@ -6,15 +6,16 @@ import com.sastartup.demo.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class UserController {
 
-    @GetMapping("/")
-    public String hello(){
-        return "startups/index";
-    }
+//    @GetMapping("/")
+//    public String hello(){
+//        return "index";
+//    }
 
     @GetMapping("/register")
     public String registerForm(Model model){
@@ -22,17 +23,20 @@ public class UserController {
         return "users/signup-form";
     }
 
-    @GetMapping("/create/startup")
-    public String createStartupForm(Model model){
-        model.addAttribute("startup", new Startup());
-        return "users/create-startup";
-    }
+//    @PostMapping("/register")
+//    public String
 
-    @GetMapping("/submitresume")
-    public String resumeForm(Model model){
-        model.addAttribute("resume", new Resume());
-        return "users/signup-form";
-    }
+//    @GetMapping("/create/startup")
+//    public String createStartupForm(Model model){
+//        model.addAttribute("startup", new Startup());
+//        return "users/create-startup";
+//    }
+//
+//    @GetMapping("/submitresume")
+//    public String resumeForm(Model model){
+//        model.addAttribute("resume", new Resume());
+//        return "users/resume";
+//    }
 
 
 }
