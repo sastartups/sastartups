08b79a6cc9d808c2105ddea816b36ce9b96d756a
Arash <arash.arghavan@gmail.com>
2019-05-14 14:58:13 -0500

    startup page and job page is added

--- src/main/java/com/sastartup/demo/controllers/StartupController.java
+++ src/main/java/com/sastartup/demo/controllers/StartupController.java
@@ -2,12 +2,34 @@ package com.sastartup.demo.controllers;
 
 
 import com.sastartup.demo.models.Job;
+import com.sastartup.demo.models.Startup;
+import com.sastartup.demo.repositories.JobRepo;
+import com.sastartup.demo.repositories.ResumeRepo;
+import com.sastartup.demo.repositories.StartupRepo;
+import com.sastartup.demo.repositories.UserRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class StartupController {
+
+
+    private final JobRepo jobDao;
+    private final ResumeRepo resumeDao;
+    private final StartupRepo startupDao;
+    private final UserRepo userDao;
+
+//    constructor
+
+
+    public StartupController(JobRepo jobDao, ResumeRepo resumeDao, StartupRepo startupDao, UserRepo userDao) {
+        this.jobDao = jobDao;
+        this.resumeDao = resumeDao;
+        this.startupDao = startupDao;
+        this.userDao = userDao;
+    }
 
 
     @GetMapping("/create-job-posting")
@@ -15,4 +37,25 @@ public class StartupController {
         model.addAttribute("job", new Job());
         return "startups/create-job-posting";
     }
+
+
+    //    shows all the startup  in the table
+    @GetMapping("/showpage")
+    public String showpage(Model vmodel){
+        vmodel.addAttribute("allstartups",startupDao.findAll());
+        return "startups/showpage";
+    }
+
+
+    //    show one startup and details
+    @GetMapping("/showpage/{id}")
+    public String showOne(@PathVariable Long id,Model vmodel){
+        Startup startup = startupDao.findOne(id);
+        vmodel.addAttribute("oneStartup",startup);
+        return "startups/showone";
+    }
+
+
+
+
 }
