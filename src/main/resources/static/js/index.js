d82120c16a5d98d7a68c7a6e80d976baef53396a
JoshMarkasky <markasky.Josh@gmail.com>
2019-05-15 22:48:44 -0600

    setting up elemets in main index page for anitmation with js and css

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,37 @@
+window.setInterval(function(){
+    if(document.documentElement["clientWidth"],
+        document.body["scrollWidth"],
+        document.documentElement["scrollWidth"],
+        document.body["offsetWidth"],
+    document.documentElement["offsetWidth"] >= 767){
+        window.onscroll = function() {animation(), animation2(), animation3(), animation4()};
+    } else {
+
+    }
+}, 300);
+
+function animation(){
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("element1").classList.remove("hidden");
+    }
+}
+
+function animation2() {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        document.getElementById("element2").classList.remove("hidden");
+    }
+}
+
+function animation3() {
+    if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+        document.getElementById("element3").classList.remove("hidden");
+    }
+}
+
+function animation4(){
+    if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
+        document.getElementById("card1").classList.remove("hidden");
+        document.getElementById("card2").classList.remove("hidden");
+        document.getElementById("card3").classList.remove("hidden");
+    }
+}
