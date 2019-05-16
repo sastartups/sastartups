6dd2143cc0e686569f26a5cfa5f32d9df7412ffb
laroblesaguilar <laroblesaguilar@gmail.com>
2019-05-16 12:50:33 -0500

    user profile updated

--- src/main/java/com/sastartup/demo/repositories/ResumeRepo.java
+++ src/main/java/com/sastartup/demo/repositories/ResumeRepo.java
@@ -1,9 +1,16 @@
 package com.sastartup.demo.repositories;
 
 import com.sastartup.demo.models.Resume;
+import com.sastartup.demo.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ResumeRepo extends CrudRepository<Resume, Long> {
+    List<Resume>findByOwner(User user);
+    Resume findByOwnerId(Long id);
+
+
 }
