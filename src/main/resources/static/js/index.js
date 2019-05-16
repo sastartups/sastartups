24715b5fd62cd451d0fcc3342591909319c5af56
Josh Markasky <markasky.Josh@gmail.com>
2019-05-16 11:09:49 -0500

    working on main index page, finshed animations and working on footer while lining elements on page up

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -13,18 +13,21 @@ window.setInterval(function(){
 function animation(){
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
         document.getElementById("element1").classList.remove("hidden");
+        document.getElementById("element1").className = "left";
     }
 }
 
 function animation2() {
     if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
         document.getElementById("element2").classList.remove("hidden");
+        document.getElementById("element2").className = "right";
     }
 }
 
 function animation3() {
     if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
         document.getElementById("element3").classList.remove("hidden");
+        document.getElementById("element3").className = "left";
     }
 }
 
