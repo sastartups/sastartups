37b5432a7d47696fe3bc0b40fce64335d98ad702
Arash <arash.arghavan@gmail.com>
2019-05-16 12:53:12 -0500

    edit and delete added to show one page

--- src/main/java/com/sastartup/demo/repositories/ResumeRepo.java
+++ src/main/java/com/sastartup/demo/repositories/ResumeRepo.java
@@ -1,9 +1,12 @@
 package com.sastartup.demo.repositories;
 
 import com.sastartup.demo.models.Resume;
+import com.sastartup.demo.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResumeRepo extends CrudRepository<Resume, Long> {
+    Resume findByOwner(User user);
+
 }
