9b16847249d5693aeea16a6095d5236a5f6f6252
Josh Markasky <markasky.Josh@gmail.com>
2019-05-23 14:50:10 -0500

    added styling to main page

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -20,21 +20,21 @@ window.setInterval(function(){
 function animation(){
     if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 200) {
         document.getElementById("element1").classList.remove("hidden");
-        document.getElementById("element1").className = "left";
+        document.getElementById("element1").className = "show";
     }
 }
 
 function animation2() {
     if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 700) {
         document.getElementById("element2").classList.remove("hidden");
-        document.getElementById("element2").className = "right";
+        document.getElementById("element2").className = "show";
     }
 }
 
 function animation3() {
     if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1100) {
         document.getElementById("element3").classList.remove("hidden");
-        document.getElementById("element3").className = "left";
+        document.getElementById("element3").className = "show";
     }
 }
 
